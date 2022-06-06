f0f7979147513d2a343bd0c10762530ddad07bcb
Arnas Mitkevičius <arnasmit@gmail.com>
2022-06-06 19:43:45 +0300

    Initial commit

---
+++ src/main/kotlin/com/harrontech/ec_records/EcRecordsApplication.kt
@@ -0,0 +1,11 @@
+package com.harrontech.ec_records
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EcRecordsApplication
+
+fun main(args: Array<String>) {
+	runApplication<EcRecordsApplication>(*args)
+}
